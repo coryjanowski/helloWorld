c1213fd8f0c4609643fe29704d0b868dc34593cb
Cory Janowski <Coryjanowski@gmail.com>
2023-10-29 15:39:09 -0600

    First update, add dog info

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello User!");
+// Create Variables
+string name = "Shadow";
+string breed = "Golden Retriever";
+int age = 5;
+double weight = 65.22;
+bool spayed = true;
+// Print variables to the console
+Console.WriteLine(name);
+Console.WriteLine(breed);
+Console.WriteLine(age);
+Console.WriteLine(weight);
+Console.WriteLine(spayed);
